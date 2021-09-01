971941f4112aba3b816e69f50895765d63ee1610
ofirgeller <ofirgeller22@gmail.com>
2021-09-01 16:46:01 +0300

    createReducer type also changed to AnyAction

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -3,7 +3,7 @@ import { Reducer, AnyAction } from 'redux';
 import { flatten } from './flatten';
 
 
-export function createReducer<TState, TAction extends IAction<any> = IAction<any>>(initState: TState, ...actions: TAction[]): Reducer<TState, TAction> {
+export function createReducer<TState, TAction extends IAction<any> = IAction<any>>(initState: TState, ...actions: TAction[]): Reducer<TState, AnyAction> {
     const _actions = {};
     flatten(actions).forEach(a => _actions[a.type] = a);
     return (state: TState, action): TState => (
